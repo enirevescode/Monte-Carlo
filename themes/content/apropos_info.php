c9c8ffae11617385efa5a3917ee5b6a2b979cd22
enirevescode <enirevescode@gmail.com>
2023-07-21 15:29:43 +0200

    content/apropos

---
+++ themes/content/apropos_info.php
@@ -0,0 +1,55 @@
+
+//vignettes/cards en dessous de l'image de la page à propos
+<br>
+<br>
+<br>
+<br>
+<div class="container mb-5">
+  <div class="card mb-3" style="max-width: 1440px;">
+    <div class="row g-0">
+        <div class="col-md-4">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/chambre.webp" class="img-fluid rounded-start" alt="suite jr">
+        </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">PRIX ET DISTINCTIONS :</h5>
+          <p class="card-text mb-3">Prix Hotel & Lodge Awards 2022 - « Hotel & Lodge Green »<br>
+          Certification « Green Globe » depuis 2014 avec obtention du « Gold Standard » en 2018<br>
+          Prix Villégiature 2017 - « Meilleure piscine d’hôtel en Europe »<br>
+          Prix Villégiature 2017 - « Meilleur groupe hôtelier en Europe »<br>
+          Certificat d’excellence TripAdvisor de 2013 à 2018<br>
+          Prix de l’Environnement Orlane – Relais & Châteaux 2015<br>
+          Certification « Silver Standard » décernée par « Welcome Chinese » 2014<br>
+          Certification Bio 2013 - attribuée par Ecocert pour le restaurant Elsa, catégorie 3<br>
+          Prix Villégiature 2012 - « Meilleure architecture intérieure d’hôtel en Europe »<br>
+          Grand Prix du Meilleur hôtel de Charme en Europe, Prix Villégiature Silver Adrian Award from Hotel Sales & Marketing Association International 2010</p>
+        </div>
+      </div>
+    </div><!--/row-->
+  </div><!--/card1-->
+  <br>
+  <br>
+  <div class="card mb-3" style="max-width: 1440px;">
+    <div class="row g-0">
+        <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">PRIX ET DISTINCTIONS :</h5>
+          <p class="card-text">Prix Hotel & Lodge Awards 2022 - « Hotel & Lodge Green »<br>
+          Certification « Green Globe » depuis 2014 avec obtention du « Gold Standard » en 2018<br>
+          Prix Villégiature 2017 - « Meilleure piscine d’hôtel en Europe »<br>
+          Prix Villégiature 2017 - « Meilleur groupe hôtelier en Europe »<br>
+          Certificat d’excellence TripAdvisor de 2013 à 2018<br>
+          Prix de l’Environnement Orlane – Relais & Châteaux 2015<br>
+          Certification « Silver Standard » décernée par « Welcome Chinese » 2014<br>
+          Certification Bio 2013 - attribuée par Ecocert pour le restaurant Elsa, catégorie 3<br>
+          Prix Villégiature 2012 - « Meilleure architecture intérieure d’hôtel en Europe »<br>
+          Grand Prix du Meilleur hôtel de Charme en Europe, Prix Villégiature Silver Adrian Award from Hotel Sales & Marketing Association International 2010</p>
+        </div>
+        </div>
+    
+        <div class="col-md-4">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/spa_zen.webp" class="img-fluid rounded-end" alt="zenitude">
+        </div>
+    </div><!--/row2-->
+  </div><!--card2-->
+</div><!-- /.container -->
